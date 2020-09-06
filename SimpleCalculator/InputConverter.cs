a8b16ce0f2707076b515c52eacd48f61925c3ea3
Olivier Matte <oliviermatte1@yahoo.ca>
2020-09-06 18:43:41 -0400

    change 1 try again

--- SimpleCalculator/InputConverter.cs
+++ SimpleCalculator/InputConverter.cs
@@ -11,7 +11,7 @@ namespace SimpleCalculator
         public double ConvertInputToNumeric(string argTextInput)
         {
             double convertedNumber;
-            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value.");
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value");
 
             return convertedNumber;
         }
